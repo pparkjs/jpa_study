8123a9f7da499eb4027740140ce79bad76996a81
pparkjs <qkrwjdtn963@gmail.com>
2024-01-04 15:50:44 +0900

    hibernate5Module 테스트 Bean 추가

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -1,7 +1,11 @@
 package jpabook.jpashop;
 
+import com.fasterxml.jackson.datatype.hibernate5.jakarta.Hibernate5JakartaModule;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class JpashopApplication {
@@ -10,4 +14,8 @@ public class JpashopApplication {
 		SpringApplication.run(JpashopApplication.class, args);
 	}
 
+//	@Bean
+//	Hibernate5JakartaModule hibernate5Module() {
+//		return new Hibernate5JakartaModule();
+//	}
 }
