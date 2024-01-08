096f4c8fe2a0a34665b8cef07ad644dc7ebb50b4
pparkjs <qkrwjdtn963@gmail.com>
2024-01-08 17:53:52 +0900

    주문 조회 V5: JPA에서 DTO 직접 조회 - 컬렉션 조회 최적화

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -104,10 +104,22 @@ public class OrderRepository {
     public List<Order> findAllWithItem() {
         return em.createQuery(
                         "select distinct o from Order o" +
-                                " join fetch o.member m" +
-                                " join fetch o.delivery d" +
-                                " join fetch o.orderItems oi" +
+                                " join fetch o.member m" + // toOne
+                                " join fetch o.delivery d" + // toOne
+                                " join fetch o.orderItems oi" + // toMany fetch join 하면 안됨
                                 " join fetch oi.item i", Order.class)
+                .setFirstResult(1)
+                .setMaxResults(100)
+                .getResultList();
+    }
+
+    public List<Order> findAllWithMemberDelivery(int offset, int limit) {
+        return em.createQuery(
+                "select o from Order o" +
+                        " join fetch o.member m" +
+                        " join fetch o.delivery d", Order.class)
+                .setFirstResult(offset)
+                .setMaxResults(limit)
                 .getResultList();
     }
 }
